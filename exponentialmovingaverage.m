ad3bd32515669e7c153575b69c712ab23b5d72a5
Kieron <chowkieron@gmail.com>
2018-01-08 18:35:35 +0000

    .

--- exponentialmovingaverage.m
+++ exponentialmovingaverage.m
@@ -2,11 +2,8 @@
 %Loading data to evaluate 
 importfile('BTC-USD.csv');
 %%
-%Plotting figure for the closing price
-[lead,lag] = movavg(Close,5,20,'e');
-createfigure([Close, lead, lag], [s, w]);
-
 %Using exponential moving average to predict to buy in or sell out
+[lead,lag] = movavg(Close,5,20,'e');
 s =zeros(size(Close));
 s(lead<=lag) = -1;             %Sell               
 s(lead>lag) =1;                %Buy
@@ -14,7 +11,9 @@ r = [0; s(1:end-1).*diff(Close)];      %Return
 w = cumsum(r);
 sh = sqrt(250)*sharpe(r,0);        %Annual Sharp Ratio 
                                    %250trading day per year
-
+%%
+%Plotting figure for the closing price
+createfigure([Close, lead, lag], [s, w]);
 %%
 %Best Parameter for lead and lag value
 annualScaling = sqrt(250);
